8ed435489e8862123ccb5fb89b83bc1d2cf0ddb5
Vesa Lukkarila <github@vesalukkarila.com>
2022-12-30 00:25:36 +0200

    feat: name as command line parameter

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"log"
+	"os"
 
 	"github.com/vesalukkarila/go-practice-packaging/greet"
 )
 
 func main() {
-	greeting, err := greet.Greet("Vesa")
+
+	name, err := getName(os.Args)
 	if err != nil {
-		log.Panic(err)
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	greeting, err := greet.Greet(name)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
 
 	}
 	fmt.Println(greeting)
+}
+
+func getName(args []string) (string, error) {
+	if len(args) < 2 {
+		return "", errors.New("name argument not provided")
+
+	}
+
+	return args[1], nil
+
 }
